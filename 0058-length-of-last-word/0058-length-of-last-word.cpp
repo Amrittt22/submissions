794dee1b304938a173dc0894d32d99e04e931a2a
Amritanshu Kumar Singh <amritanshusanjeev@gmail.com>
2026-09-12 16:14:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.29%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int count = 0;
+        for(int i=n-1;i>=0;i--){
+            if(count == 0 && s[i]==' ') continue;
+            if(count !=0 && s[i]==' ') break;
+            count++;
+        }
+        return count;
+    }
+};
